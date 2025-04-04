9aa6a6b9674a575ace09d754b1c1dccca2b6da36
Rwin97 <erwinmarioleonramos@gmail.com>
2025-04-04 09:15:34 -0400

    actulizacion

--- src/components/MultipleInput.jsx
+++ src/components/MultipleInput.jsx
@@ -1,24 +1,20 @@
-const MultipleInput = ({ theme, handlerSelectClick, handlerSelectClick2, label, name1, name2, value1, value2, required1 = false, required2 = false }) => {
+const MultipleInput = ({ theme, handlerSelectClick, handlerSelectClick2, label, name1, name2, defaultValue1, defaultValue2, required1 = false, required2 = false }) => {
     return (
         <div className='flex justify-end items-center'>
             <label htmlFor="" className={`mr-2 w-14 text-[10px] ${theme === 'light' ? ' text-gray-950' : ' text-gray-950 '} dark:text-white`}>
                 {label}
             </label>
             <div className='grid grid-cols-2 gap-2'>
-                <input 
-                    type='date' 
-                    className="h-[25px] max-w-[173px] w-full px-2 border border-gray-400 rounded-[5px] text-[10px] text-gray-950" 
+                <input type='date' className="h-[25px] max-w-[173px] w-full px-2 border border-gray-400 rounded-[5px] text-[10px] text-gray-950" 
                     name={name1} 
                     onChange={handlerSelectClick} 
-                    value={value1}  // Cambiado de defaultValue a value
+                    defaultValue={defaultValue1}   
                     required={required1} 
                 />
-                <input 
-                    type='date' 
-                    className="h-[25px] max-w-[173px] w-full px-2 border border-gray-400 rounded-[5px] text-[10px] text-gray-950" 
+                <input type='date' className="h-[25px] max-w-[173px] w-full px-2 border border-gray-400 rounded-[5px] text-[10px] text-gray-950" 
                     name={name2} 
                     onChange={handlerSelectClick2} 
-                    value={value2}  // Cambiado de defaultValue a value
+                    defaultValue={defaultValue2}   
                     required={required2} 
                 />
             </div>
